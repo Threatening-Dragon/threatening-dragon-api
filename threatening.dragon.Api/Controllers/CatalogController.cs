d6c6754eacbb0591c28de4e841d244515b3f1c65
Zachary Mason <mason.994@osu.edu>
2022-04-06 18:43:25 -0400

    completes part 2 - 3: adding the db to the file and updates

--- threatening.dragon.Api/Controllers/CatalogController.cs
+++ threatening.dragon.Api/Controllers/CatalogController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using threatening.dragon.Domain.Catalog;
+using threatening.dragon.Data;
 
 namespace threatening.dragon.Api.Controllers
 {
@@ -7,16 +8,17 @@ namespace threatening.dragon.Api.Controllers
     [Route("/catalog")]
     public class CatalogController : ControllerBase
     {
+        private readonly StoreContext _db;
+
+        public CatalogController(StoreContext db)
+        {
+            _db = db;
+        }
+
         [HttpGet]
         public IActionResult GetItems()
         {
-            var items = new List<Item>()
-            {
-                new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m),
-                new Item("Shorts", "Ohio State shorts.", "Nike", 44.99m)
-            };
-
-            return Ok(items);
+            return Ok(_db.Items);
         }
 
         [HttpGet("{id:int}")]
--- threatening.dragon.Api/Program.cs
+++ threatening.dragon.Api/Program.cs
@@ -1,25 +1,16 @@
+using threatening.dragon.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<StoreContext>(options => 
+    options.UseSqlite("Data Source=../Registrar.sqlite",
+    b => b.MigrationsAssembly("threatening.dragon.Api"))
+);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
