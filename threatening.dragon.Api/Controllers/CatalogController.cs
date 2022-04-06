410b2ff62f23fb9d2bd3e0c25dfece6674a6e7d5
Zachary Mason <mason.994@osu.edu>
2022-04-06 19:26:30 -0400

    completing adding Post

--- threatening.dragon.Api/Controllers/CatalogController.cs
+++ threatening.dragon.Api/Controllers/CatalogController.cs
@@ -35,7 +35,9 @@ namespace threatening.dragon.Api.Controllers
         [HttpPost]
         public IActionResult Post(Item item)
         {
-            return Created("/catalog/42", item);
+            _db.Items.Add(item);
+            _db.SaveChanges();
+            return Created($"/catalog/{item.Id}", item);
         }
 
         [HttpPost("{id:int}/ratings")]
